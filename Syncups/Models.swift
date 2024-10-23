c89ec4c26a032feffd09006d6cf7f4948dcf167c
vanya <vanya.moo@gmail.com>
2024-10-23 17:01:30 +0100

    Add SyncupModel and SyncupList

--- Syncups/Models.swift
+++ Syncups/Models.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 import Tagged
 
 struct Syncup: Identifiable, Codable, Equatable {
@@ -51,19 +52,76 @@ enum Theme: String, CaseIterable, Identifiable, Codable {
     case teal
     case yellow
     
-//    var accentColor: Color {
-//        switch self {
-//        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-//        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
-//        }
-//    }
-//    var mainColor: Color {
-//        Color(rawValue)
-//    }
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+    
     var name: String {
         rawValue.capitalized
     }
     var id: String {
         name
     }
+}
+
+extension Syncup {
+  static let mock = Self(
+    id: Syncup.ID(UUID()),
+    attendees: [
+      Attendee(id: Attendee.ID(UUID()), name: "Blob"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob Jr"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob Sr"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob Esq"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob III"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob I"),
+    ],
+    duration: .seconds(60),
+    meetings: [
+      Meeting(
+        id: Meeting.ID(UUID()),
+        date: Date().addingTimeInterval(-60 * 60 * 24 * 7),
+        transcript: """
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure \
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt \
+          mollit anim id est laborum.
+          """
+      )
+    ],
+    theme: .orange,
+    title: "Design"
+  )
+
+  static let engineeringMock = Self(
+    id: Syncup.ID(UUID()),
+    attendees: [
+      Attendee(id: Attendee.ID(UUID()), name: "Blob"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob Jr"),
+    ],
+    duration: .seconds(60 * 10),
+    meetings: [],
+    theme: .periwinkle,
+    title: "Engineering"
+  )
+
+  static let designMock = Self(
+    id: Syncup.ID(UUID()),
+    attendees: [
+      Attendee(id: Attendee.ID(UUID()), name: "Blob Sr"),
+      Attendee(id: Attendee.ID(UUID()), name: "Blob Jr"),
+    ],
+    duration: .seconds(60 * 30),
+    meetings: [],
+    theme: .poppy,
+    title: "Product"
+  )
 }
--- Syncups/SyncupList.swift
+++ Syncups/SyncupList.swift
@@ -8,20 +8,67 @@
 import SwiftUI
 
 final class SyncupListModel: ObservableObject {
+    @Published var syncups: [Syncup]
     
+    init(syncups: [Syncup] = []) {
+        self.syncups = syncups
+    }
 }
 
 struct SyncupList: View {
+    
+    @ObservedObject var model: SyncupListModel
+    
     var body: some View {
         NavigationStack {
             List {
-                
+                ForEach(model.syncups) { syncup in
+                    CardView(syncup: syncup)
+                        .listRowBackground(syncup.theme.mainColor)
+                }
             }
             .navigationTitle("Daily Syncups")
         }
     }
 }
 
+struct CardView: View {
+    
+    let syncup: Syncup
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            title
+            Spacer()
+            HStack {
+                attendeesLabel
+                Spacer()
+                minutesLabel
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(syncup.theme.accentColor)
+    }
+    
+    private var title: some View {
+        Text(syncup.title)
+            .font(.headline)
+            .accessibilityAddTraits(.isHeader)
+    }
+    
+    private var attendeesLabel: some View {
+        Label("\(syncup.attendees.count)", systemImage: "person.3")
+            .accessibilityLabel("\(syncup.attendees.count) attendees")
+    }
+    
+    private var minutesLabel: some View {
+        Label("\(syncup.duration)", systemImage: "clock")
+            .accessibilityLabel("\(syncup.duration) minute meeting")
+            //.labelStyle(.trailingIcon)
+    }
+}
+
 #Preview {
-    SyncupList()
+    SyncupList(model: SyncupListModel(syncups: [.mock]))
 }
