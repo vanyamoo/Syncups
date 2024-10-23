d6a81df27cf8cf8d62a4fcf15962589ee138f51c
vanya <vanya.moo@gmail.com>
2024-10-23 16:01:29 +0100

    Initial Commit

---
+++ Syncups/SyncupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncupsApp.swift
+//  Syncups
+//
+//  Created by Vanya Mutafchieva on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SyncupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
