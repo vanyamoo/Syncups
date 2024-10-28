e90de10463e1167b0163235d364ae3f86b5d5f2e
vanya <vanya.moo@gmail.com>
2024-10-28 13:06:56 +0000

    Update body of SyncupDetailView

--- Syncups/SyncupDetail.swift
+++ Syncups/SyncupDetail.swift
@@ -19,7 +19,71 @@ struct SyncupDetailView: View {
     
     @ObservedObject var model: SyncupDetailModel
     var body: some View {
-        Text("Detail")
+        List {
+            Section {
+                Button {
+                    //model.startMeetingButtonTapped()
+                } label: {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text(model.syncup.duration.formatted(.units()))
+                }
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(model.syncup.theme.name)
+                        .padding(4)
+                        .foregroundColor(model.syncup.theme.accentColor)
+                        .background(model.syncup.theme.mainColor)
+                        .cornerRadius(4)
+                }
+            } header: {
+                Text("Sync-up Info")
+            }
+            
+            if !model.syncup.meetings.isEmpty {
+                Section {
+                    ForEach(model.syncup.meetings) { meeting in
+                        Button {
+                            //model.meetingTapped(meeting)
+                        } label: {
+                            HStack {
+                                Image(systemName: "calendar")
+                                Text(meeting.date, style: .date)
+                                Text(meeting.date, style: .time)
+                            }
+                        }
+                    }
+                    .onDelete { indices in
+                        //model.deleteMeetings(atOffsets: indices)
+                    }
+                } header: {
+                    Text("Past meetings")
+                }
+            }
+            
+            Section {
+                ForEach(model.syncup.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            } header: {
+                Text("Attendees")
+            }
+            
+            Section {
+                Button("Delete") {
+                    //model.deleteButtonTapped()
+                }
+                .foregroundColor(.red)
+                .frame(maxWidth: .infinity)
+            }
+        }
     }
 }
 
